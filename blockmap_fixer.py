e0cd6fb3fa93b19b557df4c29f37864b867f37cb
Giacomo 'Mr. Wolf' Furlan <giacomo@giacomofurlan.name>
2024-02-16 03:03:55 +0100

    Fix oak door facing

--- blockmap_fixer.py
+++ blockmap_fixer.py
@@ -5,16 +5,16 @@ import yaml
 blockmap_file = Path(os.getcwd()).joinpath('', 'blockmap.yaml')
 data = yaml.load(blockmap_file.read_text(), Loader=yaml.loader.BaseLoader)
 
-if int(data['version']) < 21:
-    data['version'] = 21
+if int(data['version']) < 22:
+    data['version'] = 22
     for name, block in data['blocks'].items():
-        if 'Door (Lower, Facing East' in name:
+        if 'Door (Facing East' in name:
             block['facing'] = 'west'
-        if 'Door (Lower, Facing West' in name:
+        if 'Door (Facing West' in name:
             block['facing'] = 'east'
-        if 'Door (Lower, Facing North' in name:
+        if 'Door (Facing North' in name:
             block['facing'] = 'south'
-        if 'Door (Lower, Facing South' in name:
+        if 'Door (Facing South' in name:
             block['facing'] = 'north'
 
 yaml.dump(data, blockmap_file.open('w'), sort_keys=False)
