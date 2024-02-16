6745365e508a3650969d4eca2dbb8d350a4b86a5
Giacomo 'Mr. Wolf' Furlan <giacomo@giacomofurlan.name>
2024-02-16 02:08:13 +0100

    Doors facing fix; north oak wall sign faxing fix; more no regression tests

--- blockmap_fixer.py
+++ blockmap_fixer.py
@@ -5,10 +5,16 @@ import yaml
 blockmap_file = Path(os.getcwd()).joinpath('', 'blockmap.yaml')
 data = yaml.load(blockmap_file.read_text(), Loader=yaml.loader.BaseLoader)
 
-if int(data['version']) < 20:
-    data['version'] = 20
+if int(data['version']) < 21:
+    data['version'] = 21
     for name, block in data['blocks'].items():
-        if 'bottom half' in name.lower(): block['half'] = 'bottom'
-        if 'top half' in name.lower(): block['half'] = 'top'
+        if 'Door (Lower, Facing East' in name:
+            block['facing'] = 'west'
+        if 'Door (Lower, Facing West' in name:
+            block['facing'] = 'east'
+        if 'Door (Lower, Facing North' in name:
+            block['facing'] = 'south'
+        if 'Door (Lower, Facing South' in name:
+            block['facing'] = 'north'
 
 yaml.dump(data, blockmap_file.open('w'), sort_keys=False)
--- tests/no_regression_test.py
+++ tests/no_regression_test.py
@@ -1,8 +1,18 @@
 from collections import namedtuple
+from dataclasses import dataclass, field
 from src import grabcraft
 from src.common import Coordinates
 
-BlockCheck = namedtuple('BlockCheck', ['coordinates', 'name', 'facing', 'half'])
+
+@dataclass
+class BlockCheck:
+    coordinates: tuple[int, int, int]
+    name: str
+    facing: str
+    half: str = field(default=None)
+    type: str = field(default=None)
+    open: str = field(default=None)
+    hinge: str = field(default=None)
 
 
 def assert_block_state(bp_url: str, elements: list[BlockCheck]):
@@ -12,6 +22,7 @@ def assert_block_state(bp_url: str, elements: list[BlockCheck]):
         assert definition.blocks[Coordinates(*elem.coordinates)].get('name') == elem.name
         assert definition.blocks[Coordinates(*elem.coordinates)].get('facing') == elem.facing
         assert definition.blocks[Coordinates(*elem.coordinates)].get('half') == elem.half
+        assert definition.blocks[Coordinates(*elem.coordinates)].get('type') == elem.type
 
 
 # Example of ↑ West, → North, ↓ East, ← South blueprint
@@ -37,5 +48,48 @@ def test_wnes_large_medieval_town_house_1():
             BlockCheck((6, 4, 5), 'minecraft:stone_brick_stairs', 'east', 'top'),
             BlockCheck((6, 5, 4), 'minecraft:spruce_stairs', 'north', 'top'),
             BlockCheck((6, 5, 5), 'minecraft:spruce_stairs', 'west', 'top'),
+        ]
+    )
+
+
+def test_wnes_fantasy_town_small_house_2():
+    assert_block_state(
+        'https://www.grabcraft.com/minecraft/fantasy-town-small-house-2/other-193',
+        [
+            # Wall sign (north only)
+            BlockCheck((3, 8, 3), 'minecraft:oak_wall_sign', 'north', None),
+
+            # Chests (single, double, all facings)
+            BlockCheck((2, 5, 16), 'minecraft:chest', 'north', None, 'left'),
+            BlockCheck((2, 6, 16), 'minecraft:chest', 'north', None, 'right'),
+            BlockCheck((2, 8, 3), 'minecraft:chest', 'north', None, None),
+            BlockCheck((2, 13, 5), 'minecraft:chest', 'south', None, 'right'),
+            BlockCheck((2, 14, 5), 'minecraft:chest', 'south', None, 'left'),
+            BlockCheck((2, 15, 14), 'minecraft:chest', 'west', None, 'right'),
+            BlockCheck((2, 15, 15), 'minecraft:chest', 'west', None, 'left'),
+            BlockCheck((6, 4, 10), 'minecraft:chest', 'east', None, 'left'),
+            BlockCheck((6, 4, 11), 'minecraft:chest', 'east', None, 'right'),
+
+            # Hopper (facing north)
+            BlockCheck((6, 13, 9), 'minecraft:hopper', 'north', None, None),
+
+            # Doors
+            BlockCheck((2, 5, 12), 'minecraft:iron_door', 'east', open='false', hinge='right'),
+            BlockCheck((3, 5, 12), 'minecraft:iron_door', 'east', open='false', hinge='right', half='upper'),
+            BlockCheck((2, 9, 4), 'minecraft:dark_oak_door', 'north', open='false', hinge='left'),
+            BlockCheck((3, 9, 4), 'minecraft:dark_oak_door', 'north', open='false', hinge='left', half='upper'),
+            BlockCheck((2, 10, 4), 'minecraft:dark_oak_door', 'north', open='false', hinge='right'),
+            BlockCheck((3, 10, 4), 'minecraft:dark_oak_door', 'north', open='false', hinge='right', half='upper'),
+            BlockCheck((6, 7, 13), 'minecraft:birch_door', 'west', open='false', hinge='left'),
+            BlockCheck((7, 7, 13), 'minecraft:birch_door', 'west', open='false', hinge='left', half='upper'),
+        ]
+    )
+
+
+def test_eswn_wild_west_sheriff_house():
+    assert_block_state(
+        'https://www.grabcraft.com/minecraft/wild-west-sheriffs-house/wooden-houses',
+        [
+            BlockCheck((4, 7, 3), 'minecraft:oak_wall_sign', 'north'),
         ]
     )
